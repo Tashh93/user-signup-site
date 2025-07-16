166595b776aa61baf7daddc0c707aa64f1d61fd5
Tashh93 <bradford.leetasha@gmail.com>
2025-07-15 23:09:17 -0500

    Working on backend.

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Tashhhhhh!</h1><p>Welcome to the server!</p>');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+
